fff5e60a9e24594c95eb0e266b785b9cd82b782a
NatalieGichuki <nataliegichuki@gmail.com>
2025-03-19 13:34:05 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+const main = document.querySelector("main#main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+
+newHeader.textContent = "Natalie is the champion";
+
+document.body.appendChild(header);
